b2c3f99ad2dd38b7f6999517b81edf86b7f455b8
alu0101543529 <alu0101543529@users.noreply.github.com>
2025-04-17 19:52:28 +0000

    deploy: 00b14eb8fcafe87e9ec052dd31f6de09d58122ec

--- code-smells/files/ejemplo_mal_js/report.history.js
+++ code-smells/files/ejemplo_mal_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Thu, 17 Apr 2025 19:33:15 GMT","sloc":52,"lloc":20,"functions":1,"deliveredBugs":0.192,"difficulty":20.16,"maintainability":53.557,"lintErrors":[]}]
+__history = [{"date":"Thu, 17 Apr 2025 19:52:27 GMT","sloc":52,"lloc":20,"functions":1,"deliveredBugs":0.192,"difficulty":20.16,"maintainability":53.557,"lintErrors":[]}]
--- code-smells/report.history.js
+++ code-smells/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Thu, 17 Apr 2025 19:33:16 GMT","total":{"sloc":118,"maintainability":128.9},"average":{"sloc":59,"maintainability":"64.45"}}]
+__history = [{"date":"Thu, 17 Apr 2025 19:52:27 GMT","total":{"sloc":118,"maintainability":128.9},"average":{"sloc":59,"maintainability":"64.45"}}]
