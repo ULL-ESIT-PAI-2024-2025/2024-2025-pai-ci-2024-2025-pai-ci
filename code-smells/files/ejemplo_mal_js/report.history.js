e83a055dab414f1b6b57803497dac99f0ab8cdff
naileacg <naileacg@users.noreply.github.com>
2025-04-17 23:45:31 +0000

    deploy: 7b971d69b1ddf5e29cc9d30f93388eed071808ba

--- code-smells/files/ejemplo_mal_js/report.history.js
+++ code-smells/files/ejemplo_mal_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Thu, 17 Apr 2025 23:33:42 GMT","sloc":52,"lloc":20,"functions":1,"deliveredBugs":0.192,"difficulty":20.16,"maintainability":53.557,"lintErrors":[]}]
+__history = [{"date":"Thu, 17 Apr 2025 23:45:31 GMT","sloc":52,"lloc":20,"functions":1,"deliveredBugs":0.192,"difficulty":20.16,"maintainability":53.557,"lintErrors":[]}]
--- code-smells/report.history.js
+++ code-smells/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Thu, 17 Apr 2025 23:33:42 GMT","total":{"sloc":118,"maintainability":128.9},"average":{"sloc":59,"maintainability":"64.45"}}]
+__history = [{"date":"Thu, 17 Apr 2025 23:45:31 GMT","total":{"sloc":118,"maintainability":128.9},"average":{"sloc":59,"maintainability":"64.45"}}]
