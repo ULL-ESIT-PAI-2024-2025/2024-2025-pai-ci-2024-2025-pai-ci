8f54ab5cdef42c38b87b3190f219e6ec21bc618f
frannelfran <frannelfran@users.noreply.github.com>
2025-04-17 17:03:02 +0000

    deploy: 20e366f75b51ff906c8764d6cef9575d6d709f99

--- code-smells/files/ejemplo_bien_js/report.history.js
+++ code-smells/files/ejemplo_bien_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Thu, 17 Apr 2025 16:55:20 GMT","sloc":66,"lloc":19,"functions":4,"deliveredBugs":0.185,"difficulty":21.42,"maintainability":75.343,"lintErrors":[]}]
+__history = [{"date":"Thu, 17 Apr 2025 17:03:02 GMT","sloc":66,"lloc":19,"functions":4,"deliveredBugs":0.185,"difficulty":21.42,"maintainability":75.343,"lintErrors":[]}]
--- code-smells/report.history.js
+++ code-smells/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Thu, 17 Apr 2025 16:55:20 GMT","total":{"sloc":118,"maintainability":128.9},"average":{"sloc":59,"maintainability":"64.45"}}]
+__history = [{"date":"Thu, 17 Apr 2025 17:03:02 GMT","total":{"sloc":118,"maintainability":128.9},"average":{"sloc":59,"maintainability":"64.45"}}]
