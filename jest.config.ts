cc1425fd2dbbbfb7794406093a2c9e43086368f0
usuario <frannelfran@gmail.com>
2025-04-20 13:20:29 +0000

    feat: Poner el fichero de configuración de jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,4 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1', // Permite resolver imports con extensión `.js`
-  },
 };
